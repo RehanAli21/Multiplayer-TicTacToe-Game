4685bc9e1eea721bf973c8b5a61509c749861bc4
Rehan Ali <rehan.shah70@yahoo.com>
2021-02-06 21:05:15 +0500

    Welcome page is complete and routes added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Welcome from './Components/Welcome'
+import Local from './Components/Local'
+import Friend from './Components/Friend'
+import Random from './Components/Random'
 
 const App = () => {
-	return <Welcome />
+	return (
+		<Router>
+			<Switch>
+				<Route path={'/'} exact component={Welcome} />
+				<Route path={'/local'} component={Local} />
+				<Route path={'/friend'} component={Friend} />
+				<Route path={'/random'} component={Random} />
+			</Switch>
+		</Router>
+	)
 }
 
 export default App
--- front-end/src/Components/Welcome.js
+++ front-end/src/Components/Welcome.js
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Welcome = () => {
+	const [name, setName] = useState('')
+
 	return (
 		<div className='welcome'>
 			<h1>Welcome to Tic Tac Toe Game</h1>
 			<div>
-				<input type='text' placeholder='Enter your name...' />
-				<input type='button' value='Play Local' />
-				<input type='button' value='Play with friends' />
-				<input type='button' value='Play with random person' />
+				<input
+					type='text'
+					onChange={e => setName(e.target.value)}
+					placeholder='Enter your name...'
+				/>
+				<Link
+					onClick={e => (!name ? e.preventDefault() : null)}
+					to={`/local?name=${name}`}>
+					<input type='button' value='Play Local' />
+				</Link>
+				<Link
+					onClick={e => (!name ? e.preventDefault() : null)}
+					to={`/friend?name=${name}`}>
+					<input type='button' value='Play with friends' />
+				</Link>
+				<Link
+					onClick={e => (!name ? e.preventDefault() : null)}
+					to={`/random?name=${name}`}>
+					<input type='button' value='Play with random person' />
+				</Link>
 			</div>
 			<p>Created by Rehan Ali</p>
 		</div>
