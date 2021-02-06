afd54157e4e79e807f631db0179ff945cc0a1042
Rehan Ali <rehan.shah70@yahoo.com>
2021-02-06 21:27:59 +0500

    Updated the Welcome Component and removed query-string

--- front-end/src/Components/Welcome.js
+++ front-end/src/Components/Welcome.js
@@ -1,31 +1,18 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Welcome = () => {
-	const [name, setName] = useState('')
-
 	return (
 		<div className='welcome'>
 			<h1>Welcome to Tic Tac Toe Game</h1>
 			<div>
-				<input
-					type='text'
-					onChange={e => setName(e.target.value)}
-					placeholder='Enter your name...'
-				/>
-				<Link
-					onClick={e => (!name ? e.preventDefault() : null)}
-					to={`/local?name=${name}`}>
+				<Link style={{ textDecoration: 'none' }} to='/local'>
 					<input type='button' value='Play Local' />
 				</Link>
-				<Link
-					onClick={e => (!name ? e.preventDefault() : null)}
-					to={`/friend?name=${name}`}>
+				<Link style={{ textDecoration: 'none' }} to='/friend'>
 					<input type='button' value='Play with friends' />
 				</Link>
-				<Link
-					onClick={e => (!name ? e.preventDefault() : null)}
-					to={`/random?name=${name}`}>
+				<Link style={{ textDecoration: 'none' }} to='/random'>
 					<input type='button' value='Play with random person' />
 				</Link>
 			</div>
