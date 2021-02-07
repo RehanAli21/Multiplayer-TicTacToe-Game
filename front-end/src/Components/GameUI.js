0cb77d00cd64dba734b91262a95e7a7973a56e25
Rehan Ali <rehan.shah70@yahoo.com>
2021-02-07 13:38:09 +0500

    checking of winner by rows and columns, diagonal is pending.

--- front-end/src/Components/GameUI.js
+++ front-end/src/Components/GameUI.js
@@ -1,27 +1,74 @@
 import React from 'react'
 
-const GameUI = ({ player, setPlayer }) => {
+let grid = [
+	['', '', ''],
+	['', '', ''],
+	['', '', '']
+]
+
+const GameUI = ({ player, setPlayer, setWinner }) => {
 	const insertValue = e => {
 		if (e.target.innerHTML === '') {
 			e.target.innerHTML = `<h1 id='${e.target.id}t'>${
 				player === 'one' ? 'X' : 'O'
 			}</h1>`
 
+			const points = [
+				e.target.id.split('')[e.target.id.length - 2],
+				e.target.id.split('')[e.target.id.length - 1]
+			]
+
+			grid[points[0] - 1][points[1] - 1] = player === 'one' ? 'X' : 'O'
+
+			const winner = winnerChecker()
+			if (winner) setWinner(winner)
+
 			setPlayer(player === 'one' ? 'two' : 'one')
+		}
+	}
+
+	const winnerChecker = () => {
+		let x = 0
+		let o = 0
+
+		//for checking rows
+		for (let row = 0; row < 3; row++) {
+			for (let col = 0; col < 3; col++) {
+				if (grid[row][col] === 'X') x++
+				else if (grid[row][col] === 'O') o++
+
+				if (x === 3) return 'X'
+				else if (o === 3) return 'O'
+			}
+			x = 0
+			o = 0
+		}
+
+		//for checking columns
+		for (let row = 0; row < 3; row++) {
+			for (let col = 0; col < 3; col++) {
+				if (grid[row][col] === 'X') x++
+				else if (grid[row][col] === 'O') o++
+
+				if (x === 3) return 'X'
+				else if (o === 3) return 'O'
+			}
+			x = 0
+			o = 0
 		}
 	}
 
 	return (
 		<div className='game'>
-			<div id='game1' onClick={insertValue} className='button'></div>
-			<div id='game2' onClick={insertValue} className='button'></div>
-			<div id='game3' onClick={insertValue} className='button'></div>
-			<div id='game4' onClick={insertValue} className='button'></div>
-			<div id='game5' onClick={insertValue} className='button'></div>
-			<div id='game6' onClick={insertValue} className='button'></div>
-			<div id='game7' onClick={insertValue} className='button'></div>
-			<div id='game8' onClick={insertValue} className='button'></div>
-			<div id='game9' onClick={insertValue} className='button'></div>
+			<div id='game11' onClick={insertValue} className='button'></div>
+			<div id='game12' onClick={insertValue} className='button'></div>
+			<div id='game13' onClick={insertValue} className='button'></div>
+			<div id='game21' onClick={insertValue} className='button'></div>
+			<div id='game22' onClick={insertValue} className='button'></div>
+			<div id='game23' onClick={insertValue} className='button'></div>
+			<div id='game31' onClick={insertValue} className='button'></div>
+			<div id='game32' onClick={insertValue} className='button'></div>
+			<div id='game33' onClick={insertValue} className='button'></div>
 		</div>
 	)
 }
--- front-end/src/Components/Local.js
+++ front-end/src/Components/Local.js
@@ -3,18 +3,23 @@ import GameUI from './GameUI'
 
 const Local = () => {
 	const [player, setPlayer] = useState('one')
+	const [winner, setWinner] = useState('')
 
 	return (
 		<div className='local'>
 			<div className='names'>
-				<h2 style={{ color: player === 'one' ? 'green' : 'white' }}>
+				<h2 style={{ color: player === 'one' ? '#07E672' : 'white' }}>
 					Player 1
 				</h2>
-				<h2 style={{ color: player === 'two' ? 'green' : 'white' }}>
+				<h2 style={{ color: player === 'two' ? '#07E672' : 'white' }}>
 					Player 2
 				</h2>
 			</div>
-			<GameUI player={player} setPlayer={setPlayer} />
+			<GameUI
+				player={player}
+				setPlayer={setPlayer}
+				setWinner={setWinner}
+			/>
 		</div>
 	)
 }
